c047fa5db2718407d85d57728322b3d9201b90cd
VMansolelli <victor_vcm@hotmail.com>
2026-07-10 21:32:01 -0300

    feat: implement password hashing for user registration using bcrypt

--- back-end/index.ts
+++ back-end/index.ts
@@ -2,6 +2,7 @@ import express, { type Request, type Response } from "express";
 import { connection } from "./src/db.js";
 import { prisma } from "./src/db.js";
 import cors from "cors";
+import bcrypt from "bcrypt";
 
 const app = express();
 app.use(express.json());
@@ -39,6 +40,8 @@ app.post("/register", async (req: Request, res: Response) => {
       return res.status(400).json({ error: "All fields are required" });
     }
 
+    const hash = await bcrypt.hash(password, 10);
+
     const user = await prisma.user.findFirst({
       where: { email: email },
     });
@@ -48,11 +51,10 @@ app.post("/register", async (req: Request, res: Response) => {
     }
 
     const newUser = await prisma.user.create({
-      data: { name, email, password, cep },
+      data: { name, email, password: hash, cep },
     });
 
     res.status(201).json(newUser);
-
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal server error" });
